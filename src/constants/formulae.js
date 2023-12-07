dc240936ece62cdedb80178452a50c98b896855b
Brett McGillis <brett.mcgillis@gmail.com>
2023-12-06 21:24:10 -0500

    fix lombardi

--- src/constants/formulae.js
+++ src/constants/formulae.js
@@ -11,7 +11,7 @@ const brzycki = {
 const lombardi = {
     name: 'Lombardi',
     description: '1RM = weight lifted x reps^0.10',
-    equation: (weight, reps) => weight * (reps^0.10), 
+    equation: (weight, reps) => weight * (reps**0.10), 
 };
 
 const formulae = [
